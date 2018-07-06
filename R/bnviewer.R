922d0a761fccaa6cafd7e143e88e116586e7abe8
Robson Fernandes <robson.ux@gmail.com>
2018-07-06 00:00:12 -0300

    bnviewer project

---
+++ R/bnviewer.R
@@ -0,0 +1,10 @@
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
