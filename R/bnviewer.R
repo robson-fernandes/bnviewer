3e4eca730adf55d7e4d2deede8b09be8f6a9f5e2
Robson Fernandes <robson.ux@gmail.com>
2018-07-06 00:29:28 -0300

    bnviewer project

--- R/bnviewer.R
+++ R/bnviewer.R
@@ -8,3 +8,46 @@
 #   Build and Reload Package:  'Cmd + Shift + B'
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
+
+require(visNetwork, quietly = TRUE)
+
+plot <- function(bn.learn,
+                 visNetwork.width = "100%",
+                 visNetwork.height = "500px",
+                 visOptions.highlightNearest = TRUE,
+                 visOptions.nodesIdSelection = FALSE,
+                 edges.smooth = TRUE,
+                 edges.dashes = TRUE
+                 ){
+
+  #BN.Learn Object
+  if ("learning"  %in% names(bn.learn) &
+      "nodes"  %in% names(bn.learn) &
+      "arcs"  %in% names(bn.learn))
+  {
+
+    nodes = names(bn.learn$nodes)
+    from.collection = bn.learn$arcs[,1]
+    to.collection = bn.learn$arcs[,2]
+
+    nodes <- data.frame(id = nodes,
+                        label = paste("Node", nodes))
+
+    edges <- data.frame(from = from.collection,
+                        to = to.collection,
+                        smooth=edges.smooth,
+                        dashes=edges.dashes)
+
+    #background = '#000000'
+    visNetwork(nodes,
+               edges,
+               width = visNetwork.width,
+               height = visNetwork.height) %>%
+      visEdges(arrows = "to") %>%
+      visOptions(highlightNearest = visOptions.highlightNearest,
+                 nodesIdSelection = visOptions.nodesIdSelection) %>%
+      visLayout(randomSeed = 123)
+
+  }
+
+}
