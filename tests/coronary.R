65249088703c5ecc9d188446388ec6b47b07cc8c
matteodelucchi <mail@matteodelucchi.com>
2020-09-14 11:49:12 +0200

    fixed merge conflix - the second

--- tests/coronary.R
+++ tests/coronary.R
@@ -1,106 +1,50 @@
-<<<<<<< HEAD
-library(bnlearn)
-library(bnviewer)
-
-data(coronary)
-bn.learn.hc = hc(alarm)
-
-
-viewer(bn.learn.hc,
-     bayesianNetwork.width = "100%",
-     bayesianNetwork.height = "80vh",
-     bayesianNetwork.layout = "layout_on_grid",
-     bayesianNetwork.title="Discrete Bayesian Network - Coronary",
-     bayesianNetwork.subtitle = "Coronary heart disease data set",
-     bayesianNetwork.footer = "Fig. 1 - Layout on grid",
-     edges.smooth = TRUE,
-     node.colors = list(background = "#f4bafd",
-                        border = "#2b7ce9",
-                        highlight = "#97c2fc")
-
-    )
-
-viewer(bn.learn.hc,
-     bayesianNetwork.width = "100%",
-     bayesianNetwork.height = "80vh",
-     bayesianNetwork.layout = "layout_as_star",
-     bayesianNetwork.title="Discrete Bayesian Network - Coronary",
-     bayesianNetwork.subtitle = "Coronary heart disease data set",
-     bayesianNetwork.footer = "Fig. 1 - Layout as star",
-     edges.smooth = TRUE,
-     node.colors = list(background = "#f4bafd",
-                        border = "#2b7ce9",
-                        highlight = list(background = "#97c2fc",
-                                         border = "#2b7ce9"))
-
-)
-
-
-viewer(bn.learn.hc,
-     bayesianNetwork.width = "100%",
-     bayesianNetwork.height = "80vh",
-     bayesianNetwork.layout = "layout_in_circle",
-     bayesianNetwork.title="Discrete Bayesian Network - Coronary",
-     bayesianNetwork.subtitle = "Coronary heart disease data set",
-     bayesianNetwork.footer = "Fig. 1 - Layout with circle",
-     edges.dashes = FALSE,
-
-     node.colors = list(background = "red",
-                        border = "black",
-                        highlight = list(background = "black",
-                                         border = "red"))
-
-)
-||||||| empty tree
-=======
 library(bnlearn)
 library(bnviewer)
 
 bn.learn.hc = hc(coronary)
 
 viewer(bn.learn.hc,
-     bayesianNetwork.width = "100%",
-     bayesianNetwork.height = "80vh",
-     bayesianNetwork.layout = "layout_on_grid",
-     bayesianNetwork.title="Discrete Bayesian Network - Coronary",
-     bayesianNetwork.subtitle = "Coronary heart disease data set",
-     bayesianNetwork.footer = "Fig. 1 - Layout on grid",
-     edges.smooth = TRUE,
-     node.colors = list(background = "#f4bafd",
-                        border = "#2b7ce9",
-                        highlight = "#97c2fc")
+       bayesianNetwork.width = "100%",
+       bayesianNetwork.height = "80vh",
+       bayesianNetwork.layout = "layout_on_grid",
+       bayesianNetwork.title="Discrete Bayesian Network - Coronary",
+       bayesianNetwork.subtitle = "Coronary heart disease data set",
+       bayesianNetwork.footer = "Fig. 1 - Layout on grid",
+       edges.smooth = TRUE,
+       node.colors = list(background = "#f4bafd",
+                          border = "#2b7ce9",
+                          highlight = "#97c2fc")
 
-    )
+)
 
 viewer(bn.learn.hc,
-     bayesianNetwork.width = "100%",
-     bayesianNetwork.height = "80vh",
-     bayesianNetwork.layout = "layout_as_star",
-     bayesianNetwork.title="Discrete Bayesian Network - Coronary",
-     bayesianNetwork.subtitle = "Coronary heart disease data set",
-     bayesianNetwork.footer = "Fig. 1 - Layout as star",
-     edges.smooth = TRUE,
-     node.colors = list(background = "#f4bafd",
-                        border = "#2b7ce9",
-                        highlight = list(background = "#97c2fc",
-                                         border = "#2b7ce9"))
+       bayesianNetwork.width = "100%",
+       bayesianNetwork.height = "80vh",
+       bayesianNetwork.layout = "layout_as_star",
+       bayesianNetwork.title="Discrete Bayesian Network - Coronary",
+       bayesianNetwork.subtitle = "Coronary heart disease data set",
+       bayesianNetwork.footer = "Fig. 1 - Layout as star",
+       edges.smooth = TRUE,
+       node.colors = list(background = "#f4bafd",
+                          border = "#2b7ce9",
+                          highlight = list(background = "#97c2fc",
+                                           border = "#2b7ce9"))
 
 )
 
 
 viewer(bn.learn.hc,
-     bayesianNetwork.width = "100%",
-     bayesianNetwork.height = "80vh",
-     bayesianNetwork.layout = "layout_in_circle",
-     bayesianNetwork.title="Discrete Bayesian Network - Coronary",
-     bayesianNetwork.subtitle = "Coronary heart disease data set",
-     bayesianNetwork.footer = "Fig. 1 - Layout with circle",
-     edges.dashes = FALSE,
+       bayesianNetwork.width = "100%",
+       bayesianNetwork.height = "80vh",
+       bayesianNetwork.layout = "layout_in_circle",
+       bayesianNetwork.title="Discrete Bayesian Network - Coronary",
+       bayesianNetwork.subtitle = "Coronary heart disease data set",
+       bayesianNetwork.footer = "Fig. 1 - Layout with circle",
+       edges.dashes = FALSE,
 
-     node.colors = list(background = "red",
-                        border = "black",
-                        highlight = list(background = "black",
-                                         border = "red"))
+       node.colors = list(background = "red",
+                          border = "black",
+                          highlight = list(background = "black",
+                                           border = "red"))
 
 )
->>>>>>> 924e6786c8e589146e85c2f57866a314ea5a5fc7
--- tests/insurance.R
+++ tests/insurance.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 library(bnlearn)
 library(bnviewer)
 
@@ -22,91 +21,32 @@ viewer(structure,
        edges.smooth = FALSE,
        bayesianNetwork.layout = "layout_with_gem",
        node.colors = list(background = "#f4bafd",
-                         border = "#2b7ce9",
-                         highlight = list(background = "#97c2fc",
-                                          border = "#2b7ce9")),
+                          border = "#2b7ce9",
+                          highlight = list(background = "#97c2fc",
+                                           border = "#2b7ce9")),
 
        clusters.legend.title = list(text = "Legend"),
 
        clusters.legend.options = list(
 
-         list(label = "Driving",
-              shape = "icon",
-              icon = list(code = "f111", size = 50, color = "#e91e63")),
+          list(label = "Driving",
+               shape = "icon",
+               icon = list(code = "f111", size = 50, color = "#e91e63")),
 
-         list(label = "Cost",
-              shape = "icon",
-              icon = list(code = "f111", size = 50, color = "#03a9f4"))
+          list(label = "Cost",
+               shape = "icon",
+               icon = list(code = "f111", size = 50, color = "#03a9f4"))
        ),
 
        clusters = list(
-         list(label = "Driving",
-              shape = "icon",
-              icon = list(code = "f111", color = "#e91e63"),
-              nodes = list("DrivingSkill","DrivHist","DrivQuality")),
-         list(label = "Cost",
-              shape = "icon",
-              icon = list(code = "f111", color = "#03a9f4"),
-              nodes = list("MedCost","ILiCost","OtherCarCost"))
+          list(label = "Driving",
+               shape = "icon",
+               icon = list(code = "f111", color = "#e91e63"),
+               nodes = list("DrivingSkill","DrivHist","DrivQuality")),
+          list(label = "Cost",
+               shape = "icon",
+               icon = list(code = "f111", color = "#03a9f4"),
+               nodes = list("MedCost","ILiCost","OtherCarCost"))
        )
 
-      )
-
-data.set = subset(bnlearn::insurance, select = names(structure$nodes))
-
-bayesianNetwork.fit = bn.fit(structure, data = data.set)
-||||||| empty tree
-=======
-library(bnlearn)
-library(bnviewer)
-
-
-model.right.arrow = " Age -> (GoodStudent,RiskAversion,SocioEcon, MedCost);
-                          RiskAversion -> (DrivHist,DrivQuality);
-                          RiskAversion -> (MakeModel, SeniorTrain);
-                          MakeModel -> (Airbag, RuggedAuto, CarValue);
-                          SocioEcon -> (RiskAversion, OtherCar);
-                          SeniorTrain -> (DrivingSkill);
-                          DrivingSkill -> (DrivHist, DrivQuality);
-                          Accident -> (MedCost, ILiCost, OtherCarCost)
-                        "
-
-structure = model.to.structure(model.right.arrow)
-
-viewer(structure,
-       bayesianNetwork.width = "100%",
-       bayesianNetwork.height = "100vh",
-       # bayesianNetwork.enabled.interactive.mode = TRUE,
-       edges.smooth = FALSE,
-       bayesianNetwork.layout = "layout_with_gem",
-       node.colors = list(background = "#f4bafd",
-                         border = "#2b7ce9",
-                         highlight = list(background = "#97c2fc",
-                                          border = "#2b7ce9")),
-
-       clusters.legend.title = list(text = "Legend"),
-
-       clusters.legend.options = list(
-
-         list(label = "Driving",
-              shape = "icon",
-              icon = list(code = "f111", size = 50, color = "#e91e63")),
-
-         list(label = "Cost",
-              shape = "icon",
-              icon = list(code = "f111", size = 50, color = "#03a9f4"))
-       ),
-
-       clusters = list(
-         list(label = "Driving",
-              shape = "icon",
-              icon = list(code = "f111", color = "#e91e63"),
-              nodes = list("DrivingSkill","DrivHist","DrivQuality")),
-         list(label = "Cost",
-              shape = "icon",
-              icon = list(code = "f111", color = "#03a9f4"),
-              nodes = list("MedCost","ILiCost","OtherCarCost"))
-       )
-
-      )
->>>>>>> 924e6786c8e589146e85c2f57866a314ea5a5fc7
+)
